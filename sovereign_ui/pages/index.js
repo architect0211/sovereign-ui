0764ba7e3ce131ef3da2aa93e152fc66c231768d
Bryan E. Lloyd <youremail@project-enlighten.com>
2025-06-16 11:12:55 -0700

    Add root index page

---
+++ sovereign_ui/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>Sovereign UI Console</h1>
+      <p>Welcome to the deployment interface. Awaiting Buddy Signal...</p>
+    </div>
+  );
+}
