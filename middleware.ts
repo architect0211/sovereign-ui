f45459a4789b77b96c0418e478d2a07812a4ccc7
Bryan E. Lloyd <youremail@project-enlighten.com>
2025-07-01 19:09:24 -0700

    Temporarily disabled auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,25 @@
-// middleware.ts
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
-export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
+// export function middleware(req: NextRequest) {
+//   const basicAuth = req.headers.get('authorization')
 
-  const validUser = 'architect'
-  const validPass = 'sovereign'
+//   const validUser = 'architect'
+//   const validPass = 'SovB_piE2'
 
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pass] = atob(authValue).split(':')
+//   if (basicAuth) {
+//     const authValue = basicAuth.split(' ')[1]
+//     const [user, pass] = atob(authValue).split(':')
 
-    if (user === validUser && pass === validPass) {
-      return NextResponse.next()
-    }
-  }
+//     if (user === validUser && pass === validPass) {
+//       return NextResponse.next()
+//     }
+//   }
 
-  return new Response('Auth required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Sovereign UI"',
-    },
-  })
-}
+//   return new Response('Auth required', {
+//     status: 401,
+//     headers: {
+//       'WWW-Authenticate': 'Basic realm="Sovereign UI"',
+//     },
+//   })
+// }
